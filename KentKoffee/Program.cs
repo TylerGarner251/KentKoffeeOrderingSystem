a3fbbd7c1dae5af2b63f41db4deb358ac178352d
Tyler Garner <tylerhearngarner@gmail.com>
2022-12-08 13:11:47 +0000

    -Reduced lines of code from 568 to 225.

--- KentKoffee/Program.cs
+++ KentKoffee/Program.cs
@@ -25,93 +25,71 @@ void CostDisplay() //Displays the Cost and Information
 }
 void KentKoffee() //Runs the whole programme
 {
-
     Console.WriteLine("Welcome to Kent Koffee" + "\nPlease Select what coffee you would like by typing corresponding letter" + "\nL: Latte | C: Cappuccino | E: Espresso");
     CoffeeVar = Console.ReadLine();
-
-
-    switch (CoffeeVar.ToUpper())
-    {
+    switch (CoffeeVar.ToUpper()){
         #region Latte
         case ("L"):
             Console.Clear();
             Console.WriteLine("You have Chosen Latte | £3.85" + "\nWould you like Syrup Price | £1.00 | Please type 'y' for yes and 'n' for No");
             syrup = Console.ReadLine().ToLower();
-            switch (syrup)
-            {
+            switch (syrup){
                 case ("y"):
                     Console.WriteLine("How many would you like to order?");
                     bulk = int.Parse(Console.ReadLine());
-                    if (bulk >= 5 && bulk <= 7)
-                    {
+                    if (bulk >= 5 && bulk <= 7){
                         coffeeDisplay = "Latte";
                         DiscountDisplay = "5%";
                         Calculation = (3.85 + 1.00) * 0.95;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 8 && bulk <= 14)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 8 && bulk <= 14){
                         coffeeDisplay = "Latte";
                         DiscountDisplay = "10%";
                         Calculation = (3.85 + 1.00) * 0.90;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 15)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 15){
                         coffeeDisplay = "Latte";
                         DiscountDisplay = "15%";
                         Calculation = (3.85 + 1.00) * 0.85;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else
-                    {
+                        CostDisplay();}
+                    else{
                         coffeeDisplay = "Latte";
                         DiscountDisplay = "0%";
                         Calculation = 3.85 + 1.00;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
+                        CostDisplay();}
                     break;
                 case ("n"):
                     Console.WriteLine("How many would you like to order?");
                     bulk = int.Parse(Console.ReadLine());
-                    if (bulk >= 5 && bulk <= 7)
-                    {
+                    if (bulk >= 5 && bulk <= 7){
                         coffeeDisplay = "Latte";
                         DiscountDisplay = "5%";
                         Calculation = 3.85 * 0.95;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 8 && bulk <= 14)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 8 && bulk <= 14){
                         coffeeDisplay = "Latte";
                         DiscountDisplay = "10%";
                         Calculation = 3.85 * 0.90;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 15)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 15){
                         coffeeDisplay = "Latte";
                         DiscountDisplay = "15%";
                         Calculation = 3.85 * 0.85;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else
-                    {
+                        CostDisplay();}
+                    else{
                         coffeeDisplay = "Latte";
                         DiscountDisplay = "0%";
                         Calculation = 3.85;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    break;
-            }
+                        CostDisplay();}
+                    break;}
             break;
         #endregion
         #region Cappuccino
@@ -119,81 +97,63 @@ void KentKoffee() //Runs the whole programme
             Console.Clear();
             Console.WriteLine("You have Chosen Cappuccino | £3.85" + "\nWould you like Syrup Price | £1.00 | Please type 'y' for yes and 'n' for No");
             syrup = Console.ReadLine().ToLower();
-            switch (syrup)
-            {
+            switch (syrup){
                 case ("y"):
                     Console.WriteLine("How many would you like to order?");
                     bulk = int.Parse(Console.ReadLine());
-                    if (bulk >= 5 && bulk <= 7)
-                    {
+                    if (bulk >= 5 && bulk <= 7){
                         coffeeDisplay = "Cappuccino";
                         DiscountDisplay = "5%";
                         Calculation = (3.85 + 1.00) * 0.95;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 8 && bulk <= 14)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 8 && bulk <= 14){
                         coffeeDisplay = "Cappuccino";
                         DiscountDisplay = "10%";
                         Calculation = (3.85 + 1.00) * 0.90;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 15)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 15){
                         coffeeDisplay = "Cappuccino";
                         DiscountDisplay = "15%";
                         Calculation = (3.85 + 1.00) * 0.85;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else
-                    {
+                        CostDisplay();}
+                    else{
                         coffeeDisplay = "Cappuccino";
                         DiscountDisplay = "0%";
                         Calculation = 3.85 + 1.00;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
+                        CostDisplay();}
                     break;
                 case ("n"):
                     Console.WriteLine("How many would you like to order?");
                     bulk = int.Parse(Console.ReadLine());
-                    if (bulk >= 5 && bulk <= 7)
-                    {
+                    if (bulk >= 5 && bulk <= 7){
                         coffeeDisplay = "Cappuccino";
                         DiscountDisplay = "5%";
                         Calculation = 3.85 * 0.95;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 8 && bulk <= 14)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 8 && bulk <= 14){
                         coffeeDisplay = "Cappuccino";
                         DiscountDisplay = "10%";
                         Calculation = 3.85 * 0.90;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 15)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 15){
                         coffeeDisplay = "Cappuccino";
                         DiscountDisplay = "15%";
                         Calculation = 3.85 * 0.85;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else
-                    {
+                        CostDisplay();}
+                    else{
                         coffeeDisplay = "Cappuccino";
                         DiscountDisplay = "0%";
                         Calculation = 3.85;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    break;
-            }
+                        CostDisplay();}
+                    break;}
             break;
         #endregion
         #region Espresso
@@ -201,81 +161,63 @@ void KentKoffee() //Runs the whole programme
             Console.Clear();
             Console.WriteLine("You have Chosen Espresso | £3.35" + "\nWould you like Syrup Price | £0.80 | Please type 'y' for yes and 'n' for No");
             syrup = Console.ReadLine().ToLower();
-            switch (syrup)
-            {
+            switch (syrup){
                 case ("y"):
                     Console.WriteLine("How many would you like to order?");
                     bulk = int.Parse(Console.ReadLine());
-                    if (bulk >= 5 && bulk <= 7)
-                    {
+                    if (bulk >= 5 && bulk <= 7){
                         coffeeDisplay = "Espresso";
                         DiscountDisplay = "5%";
                         Calculation = (3.35 + 0.80) * 0.95;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 8 && bulk <= 14)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 8 && bulk <= 14){
                         coffeeDisplay = "Espresso";
                         DiscountDisplay = "10%";
                         Calculation = (3.35 + 0.80) * 0.90;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 15)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 15){
                         coffeeDisplay = "Espresso";
                         DiscountDisplay = "15%";
                         Calculation = (3.35 + 0.80) * 0.85;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else
-                    {
+                        CostDisplay();}
+                    else{
                         coffeeDisplay = "Espresso";
                         DiscountDisplay = "0%";
                         Calculation = 3.35 + 0.80;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
+                        CostDisplay();}
                     break;
                 case ("n"):
                     Console.WriteLine("How many would you like to order?");
                     bulk = int.Parse(Console.ReadLine());
-                    if (bulk >= 5 && bulk <= 7)
-                    {
+                    if (bulk >= 5 && bulk <= 7){
                         coffeeDisplay = "Espresso";
                         DiscountDisplay = "5%";
                         Calculation = 3.35 * 0.95;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 8 && bulk <= 14)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 8 && bulk <= 14){
                         coffeeDisplay = "Espresso";
                         DiscountDisplay = "10%";
                         Calculation = 3.35 * 0.90;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else if (bulk >= 15)
-                    {
+                        CostDisplay();}
+                    else if (bulk >= 15){
                         coffeeDisplay = "Espresso";
                         DiscountDisplay = "15%";
                         Calculation = 3.35 * 0.85;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    else
-                    {
+                        CostDisplay();}
+                    else{
                         coffeeDisplay = "Espresso";
                         DiscountDisplay = "0%";
                         Calculation = 3.35;
                         Total = Calculation.ToString("#.##");
-                        CostDisplay();
-                    }
-                    break;
-            }
+                        CostDisplay();}
+                    break;}
             break;
             #endregion
     }
